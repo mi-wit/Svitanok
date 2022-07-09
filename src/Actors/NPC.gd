ea487dbb143b839a6c1dee40939d454d3b04ca99
mi-wit <artakserkses.mw@gmail.com>
2022-07-09 12:43:40 +0200

    add babushka NPC dialog

--- src/Actors/NPC.gd
+++ src/Actors/NPC.gd
@@ -1,12 +1,22 @@
 extends Actor
 
+# Dialogs texts
+export var firstInteraction = [
+	"Hello, child",
+	"Go get the gooses",
+	"The storm is comming."
+] 
+
 func _ready() -> void:
 	$InteractionHint.visible = false
+	$dymek.visible = false
+	$dymek/Label.text = firstInteraction.pop_front()
 
 func _process(delta: float) -> void:
 	
 	if $InteractionHint.visible and Input.is_action_just_pressed("interaction"):
-		print("interakcja")
+		$dymek.visible = true
+		$dymek/DialogTimeOut.start()
 
 func _physics_process(delta: float) -> void:
 	if is_on_wall():
@@ -24,4 +34,13 @@ func _on_PlayerDetector_body_exited(body: Node) -> void:
 	$InteractionHint.visible = false
 
 
+func _on_DialogTimeOut_timeout() -> void:
+	$dymek.visible = false
+	var nextDialog = firstInteraction.pop_front()
+	if nextDialog:
+		$dymek/NextSentenceTimer.start()
+		$dymek/Label.text = nextDialog
 
+func _on_NextSentenceTimer_timeout() -> void:
+	$dymek.visible = true
+	$dymek/DialogTimeOut.start()
