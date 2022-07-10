4b9582deff1d81ff092aef019ebdaf85e98558d6
mi-wit <artakserkses.mw@gmail.com>
2022-07-10 09:28:40 +0200

    add outro cinematic

--- src/Levels/StormCommingLevel.gd
+++ src/Levels/StormCommingLevel.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+export var next_scene: PackedScene
+
 func _ready() -> void:
+	$CanvasLayer/VideoPlayer.visible = true
 	$CanvasLayer/VideoPlayer.play()
 
 
@@ -13,3 +16,8 @@ func _on_Timer_timeout() -> void:
 	$Thunder.play()
 	$Thunder/Timer.wait_time = rand_range(3, 15)
 	$Thunder/Timer.start()
+
+
+func _on_Area2D_body_entered(body: Node) -> void:
+	print("entered")
+	get_tree().change_scene_to(next_scene)
