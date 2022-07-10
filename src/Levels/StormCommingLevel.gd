315d8c6eca364778bbc9a3613a5fb41ce026d688
mi-wit <artakserkses.mw@gmail.com>
2022-07-10 08:45:24 +0200

    storm sound, background sounds

--- src/Levels/StormCommingLevel.gd
+++ src/Levels/StormCommingLevel.gd
@@ -6,3 +6,10 @@ func _ready() -> void:
 
 func _on_VideoPlayer_finished() -> void:
 	$CanvasLayer.queue_free()
+	$Thunder.play()
+
+
+func _on_Timer_timeout() -> void:
+	$Thunder.play()
+	$Thunder/Timer.wait_time = rand_range(3, 15)
+	$Thunder/Timer.start()
