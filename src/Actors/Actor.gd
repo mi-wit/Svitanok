194b49c1c52bcfafbdaddec9e2dff002fd7e0b1c
mi-wit <artakserkses.mw@gmail.com>
2022-07-10 10:01:56 +0200

    add bocian

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -3,7 +3,7 @@ class_name Actor
 
 const FLOOR_NORMAL: = Vector2.UP
 
-export var speed: = Vector2(500.0, 800.0)
+export var speed: = Vector2(500.0, 300.0)
 export var gravity: float = 3000.0
 
 var velocity = Vector2.ZERO
